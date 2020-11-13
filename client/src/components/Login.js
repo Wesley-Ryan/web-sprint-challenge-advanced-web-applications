84e560c727f08f602da36db9069f6bd080305118
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-11-13 10:16:53 -0800

    Completed MVP, added some styling

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,14 @@
-/** @jsxImportSource @emotion/react */
+
+
 import React from "react";
 import { useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom'
 import {login} from '../utils'
 
-import { jsx } from '@emotion/react'
+
 
 const Login = () => {
-  const { register, handleSubmit, errors, reset } = useForm({ 
+  const { register, handleSubmit, reset } = useForm({ 
     mode: 'onBlur', 
 
   });
@@ -18,29 +19,48 @@ const Login = () => {
     history.push('/bubbles')
   }
   
-  console.log(errors);
+  const Container = { 
+     margin: '0 auto',
+     marginTop: '100px',
+    width:'250px', 
+    border:'1px solid black', 
+    borderRadius: '5px', 
+    padding: '1%'
+    
+  }
+
   
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <div className='login-container'>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input type="text" 
+    <div style={{margin: '0 auto', color: 'white', background: 'black', width:'100%'}}>
+      <h1 style={{margin: '0 auto'}}>Welcome to the Bubble App!</h1>
+      <div >
+        <form onSubmit={handleSubmit(onSubmit)}
+        style={Container}>
+          <input 
+          type="text" 
           placeholder="username" 
           name="username" 
           ref={register({required: true, maxLength:   80})} 
+          style={{width:'80%', margin: '0 auto', marginTop: '10px'}}
           />
-        <input type="password" 
+        <input 
+        type="password" 
         placeholder="password" 
         name="password" 
         value='i<3Lambd4'
         ref={register({required: true, maxLength:  32})} 
+        style={{width:'80%', margin: '0 auto',marginTop: '10px' }}
         />
 
-        <input type="submit" />
+        <input type="submit" style={{width:'200px', 
+        margin: '0 auto', 
+        marginTop: '10px',
+        color:'white', 
+        background:'#6d2aff'}}
+        />
       </form>
     </div>
-    </>
+    </div>
   );
 };
 
